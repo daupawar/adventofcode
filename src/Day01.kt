731324ed6c1abf2669b010d8543c027a007eb43b
rohan <rohan.pawar@mobifilia.com>
2021-12-21 18:15:49 +0530

    day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    var last = 0
+    var counter = 0
+
+    input.forEachIndexed { index, s ->
+        if (s.toInt() > last && index != 0) {
+            counter++
+        }
+        last = s.toInt()
+    }
+    println(counter)
 }
