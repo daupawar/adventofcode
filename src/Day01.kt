8c16c86793162322edc21a7bac113a1db6c02803
rohan <rohan.pawar@mobifilia.com>
2021-12-22 18:14:37 +0530

    added text file

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,31 @@
 fun main() {
 
-    val input = readInput("Day01")
-    var last = 0
-    var counter = 0
-
-    input.forEachIndexed { index, s ->
-        if (s.toInt() > last && index != 0) {
-            counter++
-        }
-        last = s.toInt()
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
     }
-    println(counter)
+
+    fun part2(input: List<Int>): Int {
+        val win3=input.windowed(3)//group every 3
+        val win2=win3.windowed(2)// group every 2
+        return win2.count { (a, b) -> a.sum() < b.sum() }
+    }
+
+//    fun part2(input: List<Int>): Int {
+//        val win4 = input.windowed(4)//group every 3
+//        return win4.count { it[0] < it[3] }
+//    }
+
+    val input = readInputAsInt("Day01")
+//    var last = 0
+//    var counter = 0
+//
+//    input.forEachIndexed { index, s ->
+//        if (s > last && index != 0) {
+//            counter++
+//        }
+//        last = s
+//    }
+//    println(counter)
+    println(part1(input))
+    println(part2(input))
 }
